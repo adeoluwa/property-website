b90b3e594db5fd8081bc4751f18d13e853b0148a
Deolu_Josef <kiishijoseph@gmail.com>
2022-12-19 21:58:43 +0100

    merged branch with bussy, activated all links

--- prop_mang/urls.py
+++ prop_mang/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('apartment/', views.apartment, name='apartment'),
-    path('apartment_details', views.apartment_details, name='apartment_details'),
-    path('single', views.single, name='single'),
-    path('news', views.news, name='news')
+    path('apartment_details/', views.apartment_details, name='apartment_details'),
+    path('single/', views.single, name='single'),
+    path('news/', views.news, name='news')
 ]
